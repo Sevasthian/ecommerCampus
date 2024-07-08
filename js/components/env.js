3d23a704177727d9b3654eccd5e04725ceebb041
Sevasthian <160695272+Sevasthian@users.noreply.github.com>
2024-07-08 08:56:05 -0500

    feat: :bug: se hace cambio en el env

--- js/components/env.js
+++ js/components/env.js
@@ -1,4 +1,4 @@
 export const headers = {
-    'x-rapidapi-key': '95e4297c2amshb2dcf1cc6d82159p19f4bajsn1304cb1b3f5d',
-	'x-rapidapi-host': 'real-time-amazon-data.p.rapidapi.com'
+    'x-rapidapi-key': 'eb2c8bc2fbmsh7261de30f8775e0p1183e9jsn1fbbd6b50b7b',
+		'x-rapidapi-host': 'real-time-amazon-data.p.rapidapi.com'
 };
